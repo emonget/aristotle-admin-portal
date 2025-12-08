1d9246980f44c6385596fa7d34f9017e48dfe3df
Etienne M <em85@pm.me>
2025-12-08 23:39:09 +0000

    refactor: project's name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/reviews-admin-portal/',
+  base: '/digest-admin-portal/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
