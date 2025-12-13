fc6ebb1f78195248eb34149238ee3ad760347b0d
Etienne M <em85@pm.me>
2025-12-13 10:31:51 +0000

    refactor: environment to define project name and deployment url + rename project to digest-portal

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# reviews-admin-portal
+# digest-admin-portal
 interact with movie reviews stored in database
+
+## How to
+- install deps 
+```shell
+npm i
+```
+- copy [env template](.env.local.example) to `.env.local` and customize placeholder values
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/digest-admin-portal/',
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: `/${env.VITE_PROJECT_NAME}/`,
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    server: {
+      allowedHosts: [env.VITE_LOCAL_DEPLOYMENT_URL]
+    }
+  }
 })
